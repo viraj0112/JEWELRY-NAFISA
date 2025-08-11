b0218f99389a4912ad87cb619421eca87ee9ce2d
unknown <vvs6782222@gmail.com>
2025-08-11 21:38:19 +0530

    netlify env update and firebase integration

--- lib/src/auth/login_screen.dart
+++ lib/src/auth/login_screen.dart
@@ -104,7 +104,7 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: NetworkImage('https://images.unsplash.com/photo-1599354632408-963b624f1113?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'), // Example image
+            image: NetworkImage('#'), // Example image
             fit: BoxFit.cover,
           ),
         ),
